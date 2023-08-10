5acc424d954a9131cb8d2e7f9d455f9f475ff7f6
Quyet <huuquyetng@gmail.com>
2023-08-10 14:03:33 +0700

    Update Next's API routes for async button

--- packages/app/features/slices/counterSlice/fetchIdentityCount.ts
+++ packages/app/features/slices/counterSlice/fetchIdentityCount.ts
@@ -1,10 +1,13 @@
 export const fetchIdentityCount = async (amount = 1): Promise<{ data: number }> => {
-  const response = await fetch('http://localhost:3000/api/identity-count', {
+  const response = await fetch('/api/identity-count', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ amount }),
   })
   const result = await response.json()
 
+  if (response.status != 200) {
+    throw new Error('Failed to fetch data')
+  }
   return result
 }
