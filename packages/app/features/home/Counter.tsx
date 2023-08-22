0e3ca62014af96a83f59cb9111e1dcae12a2ecd5
Quyet <huuquyetng@gmail.com>
2023-08-22 23:45:02 +0700

    Change Redux to Zustand state management

--- packages/app/features/home/Counter.tsx
+++ packages/app/features/home/Counter.tsx
@@ -6,30 +6,25 @@ import {
   incrementAsync,
   incrementByAmount,
   incrementIfOddAsync,
-  selectCount,
-  selectLoading,
-  useDispatch,
-  useSelector,
-} from 'app/redux'
+  useBoundStore,
+} from 'app/zustand'
 import { Button, H1, Input, Text, XStack, YStack } from '@my/ui'
 
 export function Counter() {
-  const dispatch = useDispatch()
-  const count = useSelector(selectCount)
-  const loading = useSelector(selectLoading)
-  const [incrementAmount, setIncrementAmount] = useState('2')
+  const [incrementAmount, setIncrementAmount] = useState(2)
+  const [count, loading] = useBoundStore((state) => [state.value, state.loading])
 
   return (
     <YStack space="$4" p="$4" ai="center">
-      <H1>Redux demo</H1>
+      <H1>Zustand demo</H1>
       <XStack space="$6" jc="center" ai="center">
-        <Button size="$6" onPress={() => dispatch(increment())}>
+        <Button size="$6" onPress={() => increment()}>
           +
         </Button>
         <Text fontSize="$8" fontFamily="$body">
           {count}
         </Text>
-        <Button size="$6" onPress={() => dispatch(decrement())}>
+        <Button size="$6" onPress={() => decrement()}>
           -
         </Button>
       </XStack>
@@ -37,28 +32,22 @@ export function Counter() {
         <Input
           size="$8"
           ta="center"
-          value={incrementAmount}
+          value={`${incrementAmount}`}
           inputMode="numeric"
-          onChangeText={setIncrementAmount}
+          onChangeText={(e) => setIncrementAmount(Number(e) || 0)}
         />
         <YStack space="$4">
-          <Button
-            size="$6"
-            onPress={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}
-          >
+          <Button size="$6" onPress={() => incrementByAmount(incrementAmount)}>
             Add Amount
           </Button>
           <Button
             size="$6"
             disabled={loading !== 'idle'}
-            onPress={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
+            onPress={() => incrementAsync(incrementAmount)}
           >
             Add Async
           </Button>
-          <Button
-            size="$6"
-            onPress={() => dispatch(incrementIfOddAsync(Number(incrementAmount) || 0))}
-          >
+          <Button size="$6" onPress={() => incrementIfOddAsync(incrementAmount)}>
             Add If Odd
           </Button>
         </YStack>
