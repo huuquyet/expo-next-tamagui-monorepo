4990948cc6a9dec550fabf9538b6f7aea9edc5c6
Quyet <huuquyetng@gmail.com>
2024-06-06 17:39:24 +0700

    fix: metadata layout of next

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -1,17 +1,6 @@
-'use client'
-
-import '@tamagui/core/reset.css'
-import '@tamagui/font-inter/css/400.css'
-import '@tamagui/font-inter/css/700.css'
-import '@tamagui/font-inter/css/800.css'
-import '@tamagui/font-inter/css/900.css'
-import { config } from '@my/ui'
-import { Analytics } from '@vercel/analytics/react'
-import { Provider } from 'app/provider'
 import type { Metadata } from 'next'
-import { useServerInsertedHTML } from 'next/navigation'
 import type { ReactNode } from 'react'
-import { StyleSheet } from 'react-native'
+import { NextTamaguiProvider } from './NextTamguiProvider'
 
 const APP_TITLE = 'Tamagui Expo Next.js Monorepo App'
 const APP_DESCRIPTION = 'Tamagui, Expo, Next.js & Solito monorepo'
@@ -23,7 +12,8 @@ export const metadata: Metadata = {
   description: APP_DESCRIPTION,
   icons: '/favicon.ico',
 }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     // You can use `suppressHydrationWarning` to avoid the warning about mismatched content during hydration in dev mode
     <html lang="en" suppressHydrationWarning>
@@ -31,55 +21,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <NextTamaguiProvider>{children}</NextTamaguiProvider>
       </body>
     </html>
-  )
-}
-
-function NextTamaguiProvider({ children }: { children: ReactNode }) {
-  useServerInsertedHTML(() => {
-    // @ts-ignore
-    const rnwStyle = StyleSheet.getSheet()
-    return (
-      <>
-        <style dangerouslySetInnerHTML={{ __html: rnwStyle.textContent }} id={rnwStyle.id} />
-
-        <style
-          dangerouslySetInnerHTML={{
-            // the first time this runs you'll get the full CSS including all themes
-            // after that, it will only return CSS generated since the last call
-            __html: config.getNewCSS(),
-          }}
-        />
-
-        <style
-          dangerouslySetInnerHTML={{
-            __html: config.getCSS({
-              // if you are using "outputCSS" option, you should use this "exclude"
-              // if not, then you can leave the option out
-              exclude: process.env.NODE_ENV === 'production' ? 'design-system' : null,
-            }),
-          }}
-        />
-
-        <script
-          dangerouslySetInnerHTML={{
-            // avoid flash of animated things on enter:
-            __html: `document.documentElement.classList.add('t_unmounted')`,
-          }}
-        />
-
-        <style jsx global>{`
-          html {
-            font-family: 'Inter';
-          }
-        `}</style>
-      </>
-    )
-  })
-
-  return (
-    <Provider>
-      {children}
-      <Analytics />
-    </Provider>
   )
 }
